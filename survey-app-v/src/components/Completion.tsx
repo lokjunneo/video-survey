d69b1b89cbfe88961f01422a75a11812bcb96bfc
lokjunneo <raizutiamat@gmail.com>
2025-08-22 08:53:14 +0800

    Completion page

---
+++ survey-app-v/src/components/Completion.tsx
@@ -0,0 +1,9 @@
+export const Completion = () => {
+    return <div>
+        <div className="max-w-md w-full mx-auto bg-white shadow rounded-lg flex-col px-5 py-5 text-left">
+            <p className="text-normal font-bold mb-1">Survey Completed</ p> 
+          </div>
+
+          <br />
+    </div>
+}
